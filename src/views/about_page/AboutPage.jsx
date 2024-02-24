de5e1689c708f495e77abdfdb34465f0189d494e
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-02-24 15:26:23 -0600

    FEAT[ABOUTPAGE] Added compontents to the about page

--- src/views/about_page/AboutPage.jsx
+++ src/views/about_page/AboutPage.jsx
@@ -1,6 +1,31 @@
+import { useContext } from 'react';
+
+import { NavbarContext } from '../../context/navbar/NavbarContext';
+import { Navbar } from '../../components/navbar/Navbar';
+import { MainAbout } from './MainAbout';
+
+import '../../styles/about_page/aboutPage.css';
+import { DonateAbout } from './DonateAbout';
 
 export const AboutPage = () => {
+    const { navbarSize } = useContext(NavbarContext);
+
     return (
-        <div>AboutPage</div>
+        <div className="about-page-main-frame">
+            <Navbar />
+            <MainAbout navbarSize={navbarSize} />
+            <div 
+                className="divisor" 
+                style={
+                    {
+                        'height': `${navbarSize.height}px`, 
+                        'width': `${navbarSize.width}px`,
+                        'backgroundColor': '#fff'
+                    }
+                }
+            >
+            </div>
+            <DonateAbout navbarSize={navbarSize} />
+        </div>
     )
 }
