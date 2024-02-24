241e1e43fcd446dc1ac883db22619f5ff91b9c59
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-02-23 22:18:54 -0600

    FEAT[HOMEPAGE] Finished landing page

--- src/views/landing_page/LandingPage.jsx
+++ src/views/landing_page/LandingPage.jsx
@@ -1,6 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+import { Navbar } from '../../components/navbar/Navbar';
+import Hand from '../../../public/assets/icons/hand-holding-heart-solid.svg?react';
+
+import '../../styles/lading_page/landingPage.css';
 
 export const LandingPage = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>LandingPage</div>
+        <div className='landing-page-main-frame'>
+            <Navbar />
+            <main className='hero-banner-main-frame'>
+                <div className="hero-banner">
+                    <div className="hero-banner-txt">
+                        <h1 className='hero-banner-h1'>Un solo proposito: el cambio <span className='text-gradient'>Ayudalos</span> tranforma vidas</h1>
+                        <Hand className='hand-icon' />
+                    </div>
+                    <button 
+                        className="donate-btn"
+                        onClick={() => navigate('/catalog')}
+                    >¡Donar ahora!</button>
+                </div>
+            </main>
+        </div>
     )
 }
