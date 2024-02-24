f15cfad82627a5ca705fdc1fd49f0391dfb0d26a
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-02-23 18:28:58 -0600

    FIX[NAVBAR] Fixed bad import

--- src/AyudalosApp.jsx
+++ src/AyudalosApp.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import { Outlet } from 'react-router-dom';
 
 export const AyudalosApp = () => {
     return (
         <main>
-            Main
+            <Outlet />
         </main>
     )
 }
