1a07c1ba6edc756c1e3daabf41aef96ff229ed74
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-02-24 15:17:36 -0600

    FEAT[NAVBAR] Tweked the navbar a little

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,53 @@
+import { useContext, useLayoutEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
+import { NavbarContext } from '../../context/navbar/NavbarContext';
+
 import Heart from '../../../public/assets/icons/heart-solid.svg?react';
 
 import '../../styles/navbar/navbar.css';
 
 export const Navbar = () => {
+    const { customStyle, handleNavbarSizeChange } = useContext(NavbarContext);
+    
+    const navbar = useRef(null); 
+
+    useLayoutEffect(() => {
+        const {height, width} = navbar.current.getBoundingClientRect();
+
+        handleNavbarSizeChange({height, width});
+    }, [])
+
     return (
-        <nav className='navbar-main-frame'>
+        <nav className='navbar-main-frame' style={customStyle} ref={navbar}>
             <div className="navbar-items">
                 <p className='navbar-logo'>
-                    Ayúdalos <Heart className='heart-icon'/>
+                    Ayúdalos <Heart className='heart-icon' />
                 </p>
                 <ul className="navbar-items-list">
-                    <NavLink 
-                        to={'/'} 
-                        className={({isActive}) => `navbar-item ${isActive ? 'navbar-item-active' : ''}`} 
-                    >Inicio</NavLink> 
-                    <NavLink 
-                        to={'/informacion'} 
-                        className={({isActive}) => `navbar-item ${isActive ? 'navbar-item-active' : ''}`} 
-                    >Sobre Nosotros</NavLink> 
-                    <NavLink 
-                        to={'/catalogo'} 
-                        className={({isActive}) => `navbar-item ${isActive ? 'navbar-item-active' : ''}`} 
-                    >Catálogo</NavLink> 
-                    <NavLink 
-                        to={'/registro'} 
-                        className={({isActive}) => `navbar-item ${isActive ? 'navbar-item-active' : ''}`} 
-                    >Registro</NavLink> 
+                    <NavLink
+                        to={'/'}
+                        className={({ isActive }) => `navbar-item ${isActive ? 'navbar-item-active' : ''}`}
+                    >
+                        Inicio
+                    </NavLink>
+                    <NavLink
+                        to={'/about'}
+                        className={({ isActive }) => `navbar-item ${isActive ? 'navbar-item-active' : ''}`}
+                    >
+                        Sobre Nosotros
+                    </NavLink>
+                    <NavLink
+                        to={'/catalog'}
+                        className={({ isActive }) => `navbar-item ${isActive ? 'navbar-item-active' : ''}`}
+                    >
+                        Catálogo
+                    </NavLink>
+                    <NavLink
+                        to={'/register'}
+                        className={({ isActive }) => `navbar-item ${isActive ? 'navbar-item-active' : ''}`}
+                    >
+                        Registro
+                    </NavLink>
                 </ul>
             </div>
         </nav>
