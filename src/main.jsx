2722f949534f9321dd08ae11e8665c7422030aae
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-02-25 18:13:47 -0600

    FIX[MAIN] Restored strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -9,5 +9,7 @@ import './styles/index.css';
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
