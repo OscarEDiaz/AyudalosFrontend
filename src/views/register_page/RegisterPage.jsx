c50e14a65cb5ed16113b2ad4ad1520c861f0eb60
MaxRBudib <maximilianoromerobudib@yahoo.com.mx>
2024-03-08 23:09:36 -0600

    functionality of sending message, still missing design

--- src/views/register_page/RegisterPage.jsx
+++ src/views/register_page/RegisterPage.jsx
@@ -1,73 +1,35 @@
-import { Navbar } from '../../components/navbar/Navbar';
-import React, { useState } from 'react';
-import '../../styles/register_page/registerPage.css';
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export const RegisterPage = () => {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [orgName, setOrgName] = useState('');
-  const [telephone, setTelephone] = useState('');
-  const [socMedia, setSocMedia] = useState('');
+  const form = useRef();
 
-  const handleRegister = () => {
-    // Here you can perform registration logic, such as sending data to a server
-    console.log('Full Name:', fullName);
-    console.log('Email:', email);
-    console.log('Password:', password);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_mm6iu2q', 'template_qs7obdj', form.current, {
+        publicKey: 'lya6e-NSXmgTVrOHP',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
   };
-    return (
-        <div className='landing-page-main-frame'>
-            <Navbar />
-            <h2>Register</h2>
-            <div className='fuckshit'>
-                <form>
-                    <div>
-                        <label>Nombre de la Organización:</label>
-                        <input
-                            type="text"
-                            value={orgName}
-                            onChange={(e) => setSocMedia(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label>Nombre del Representante:</label>
-                        <input
-                            type="text"
-                            value={fullName}
-                            onChange={(e) => setFullName(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label>Correo Electrónico:</label>
-                        <input
-                            type="text"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label>Telefono:</label>
-                        <input
-                            type="text"
-                            value={telephone}
-                            onChange={(e) => setTelephone(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label>Red social:</label>
-                        <input
-                            type="email"
-                            value={socMedia}
-                            onChange={(e) => setSocMedia(e.target.value)}
-                        />
-                    </div>
-                    <button type="button" onClick={handleRegister}>
-                    Register
-                    </button>
-                </form>
-        </div>
-        </div>
-    )
-}
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
