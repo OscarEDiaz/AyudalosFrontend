0cb5fe5b60004a308a26d3bc8f7a5436827ea0fd
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-03-10 13:39:20 -0600

    FEAT[CATALOG] Updated data source of the component

--- src/views/catalog_page/IniciativeList.jsx
+++ src/views/catalog_page/IniciativeList.jsx
@@ -2,15 +2,17 @@ import { Filter, FilterTag, IniciativeCard } from './index.js';
 
 import '../../styles/catalog_page/iniciativeList.css';
 
-export const IniciativeList = ({ iniciatives, filters }) => {
+export const IniciativeList = ({ initiatives, filters }) => {
     return (
         <div className="iniciative-list-main-frame">
             <Filter />
             <div className="filter-tags-container">
-                { filters.map((tag, i) => <FilterTag key={i} tag={tag} />) }
+                { filters.map((tag, i) => <FilterTag key={i} tacag={tag} />) }
             </div>
             <div className="iniciative-cards-container">
-                { iniciatives.map((title, i) => <IniciativeCard key={i} name={title} />) }
+                {
+                    initiatives.map((data, i) => <IniciativeCard key={i} data={data} index={i} /> )
+                }
                 <div className="page-controller">
                     <button className="page-selector">Atras</button>
                     <p>Page</p>
