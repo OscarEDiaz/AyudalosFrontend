c96a3b9c007f90a5288a9e27cdec68f9d6f90ad4
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-03-10 17:45:55 -0600

    FEAT[CATALOG] Implemented filter functionality

--- src/views/catalog_page/FilterSelector.jsx
+++ src/views/catalog_page/FilterSelector.jsx
@@ -1,9 +1,37 @@
+import { useContext, useEffect, useState } from 'react';
+
+import { CatalogContext } from '../../context/catalog/CatalogContext';
+
 import '../../styles/catalog_page/filter.css';
 
 export const FilterSelector = ({ tagName }) => {
+    const [isToggled, setIsToggled] = useState(false);
+    const { addFilter, clearFilter, filters } = useContext(CatalogContext);
+
+    const handleFilterClicked = () => {
+        if (!isToggled) {
+            addFilter(tagName);
+        } else {
+            clearFilter(tagName);
+        }
+
+        setIsToggled(!isToggled);
+    };
+
+    useEffect(() => {
+        if (!filters.find((currentTag) => currentTag === tagName)) setIsToggled(false);
+    }, [filters])
+
+
     return (
-        <div className="filter-tag">
-            <p className="filter-tag-text">{tagName}</p>
+        <div
+            className={`filter-tag ${isToggled ? 'selected' : ''}`}
+            onClick={handleFilterClicked}
+        >
+            <p
+                className="filter-tag-text"
+                style={{ 'color': isToggled ? '#fff' : '' }}
+            >{tagName}</p>
         </div>
     )
 }
