ee8d4b0b0f861ee90a997a8eac39abbb6cd531e7
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-03-10 17:30:38 -0600

    FEAT[CATALOG] Removed dummy filters

--- src/views/catalog_page/Catalog.jsx
+++ src/views/catalog_page/Catalog.jsx
@@ -6,13 +6,12 @@ import { CatalogContext } from "../../context/catalog/CatalogContext.jsx";
 
 export const Catalog = ({ navbarSize }) => {
     // TODO: fetch filter tags
-    const filters = ['test', 'test', 'test'];
     const { testInfo:initiatives } = useContext(CatalogContext);
 
     return (
         <div className="catalog">
             <div className="side-section left" style={{ 'height': `calc(100vh - ${navbarSize.height}px)`}}>
-                <IniciativeList initiatives={initiatives} filters={filters} />
+                <IniciativeList initiatives={initiatives} />
             </div>
             <div className="side-section right" style={{ 'minHeight': `calc(100vh - ${navbarSize.height}px)`}}>
                 <Iniciative />
