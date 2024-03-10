a234797e05c75226da1072586dc32041102fdc29
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-03-10 13:31:19 -0600

    FEAT[CATALOG] Updated component to recieve information from catalog global context

--- src/views/catalog_page/Catalog.jsx
+++ src/views/catalog_page/Catalog.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react";
+
 import { IniciativeList, Iniciative } from "./index.js";
+
+import { CatalogContext } from "../../context/catalog/CatalogContext.jsx";
 
 export const Catalog = ({ navbarSize }) => {
     // TODO: fetch filter tags
     const filters = ['test', 'test', 'test'];
-    const iniciatives = ['test', 'test', 'test', 'test', 'test', 'test'];
-    
+    const { testInfo:initiatives } = useContext(CatalogContext);
+
     return (
         <div className="catalog">
             <div className="side-section left" style={{ 'height': `calc(100vh - ${navbarSize.height}px)`}}>
-                <IniciativeList  iniciatives={iniciatives} filters={filters} />
+                <IniciativeList initiatives={initiatives} filters={filters} />
             </div>
             <div className="side-section right" style={{ 'minHeight': `calc(100vh - ${navbarSize.height}px)`}}>
                 <Iniciative />
