0ae1ba01808179f93f670477e2ad908e525c7573
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-03-10 13:30:33 -0600

    FEAT[CATALOG] Added dummy info to test components

--- src/context/catalog/CatalogProvider.jsx
+++ src/context/catalog/CatalogProvider.jsx
@@ -1,8 +1,49 @@
+import { useState } from "react";
 import { useGet } from "../../hooks/useGet";
 import { CatalogContext } from "./CatalogContext";
 
 export const CatalogProvider = ({ children }) => {
-    const { data:initiatives, dataIsLoading:initiativesAreLoading, error:initiativesError } = useGet('/getInitiatives');
+    const { data: initiatives, dataIsLoading: initiativesAreLoading, error: initiativesError } = useGet('/api/initiatives');
+
+    const testInfo = [
+        {
+            initiativeTitle: 'Test 1',
+            organizationName: 'OrgName Test 1',
+            tags: ['tag1', 'tag2', 'tag3'],
+            smallDescription: 'Small description for Test 1',
+            detailedCard: {
+                initiativeTitle: 'Test 1 Details',
+                bigDescription: 'Detailed description for Test 1.'
+            }
+        },
+        {
+            initiativeTitle: 'Test 2',
+            organizationName: 'OrgName Test 2',
+            tags: ['tag4', 'tag5', 'tag6'],
+            smallDescription: 'Small description for Test 2',
+            detailedCard: {
+                initiativeTitle: 'Test 2 Details',
+                bigDescription: 'Detailed description for Test 2.'
+            }
+        },
+        {
+            initiativeTitle: 'Test 3',
+            organizationName: 'OrgName Test 3',
+            tags: ['tag7', 'tag8', 'tag9'],
+            smallDescription: 'Small description for Test 3',
+            detailedCard: {
+                initiativeTitle: 'Test 3 Details',
+                bigDescription: 'Detailed description for Test 3.'
+            }
+        }
+    ];
+
+    // Handle wich initiative is displayed to the right
+    const [selectedInitiativeIndex, setSelectedInitiativeIndex] = useState(0);
+
+    const changeDisplayedInitiative = (id) => {
+        setSelectedInitiativeIndex(id);
+    }
 
     return (
         <CatalogContext.Provider
@@ -10,7 +51,10 @@ export const CatalogProvider = ({ children }) => {
                 {
                     initiatives,
                     initiativesAreLoading,
-                    initiativesError
+                    initiativesError,
+                    selectedInitiativeIndex,
+                    testInfo,
+                    changeDisplayedInitiative
                 }
             }
         >
