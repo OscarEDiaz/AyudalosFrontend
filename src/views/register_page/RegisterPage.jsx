edc859ef71e273685ed7184a993b0e9bb5f3f6f5
Oscar Ramírez <oscarrmzdiaz@gmail.com>
2024-03-11 22:35:48 -0600

    FIX[DEV] Hotfix

--- src/views/register_page/RegisterPage.jsx
+++ src/views/register_page/RegisterPage.jsx
@@ -1,9 +1,13 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 import { Navbar } from '../../components/navbar/Navbar';
+
 import '../../styles/register_page/registerPage.css';
+import { useEffect } from 'react';
 
 export const RegisterPage = () => {
+  const [emailSent, setEmailSent] = useState(false);
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -11,32 +15,37 @@ export const RegisterPage = () => {
 
     emailjs
       .sendForm('service_mm6iu2q', 'template_qs7obdj', form.current, {
-        publicKey: 'lya6e-NSXmgTVrOHP',
+        publicKey: `${import.meta.env.VITE_EMAIL_JS_API_KEY}`,
       })
       .then(
         () => {
-          console.log('SUCCESS!');
+          setEmailSent(true);
         },
         (error) => {
-          console.log('FAILED...', error.text);
+          setEmailSent(false);
         },
       );
   };
 
+  useEffect(() => {
+    setEmailSent(false);
+  }, [])
+
   return (
-    <div>
+    <div className='form-div'>
       <Navbar />
-      <div className="form_holder">
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-    </div>
+      <div className="form_holder" style={{ 'display': 'flex', 'flexDirection': 'column', 'alignItems': 'center' }}>
+        <form className='register-form' ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+        { emailSent && <p style={{ 'marginTop': '20px' }}>¡Correo enviado! Te contactaremos pronto</p> }
+      </div>
     </div>
   );
 };
